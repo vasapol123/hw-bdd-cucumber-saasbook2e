e68de127132eae758a457e0c096160a2fdf4c744
ThanawatKMUTNB <s6301012620111@email.kmutnb.ac.th>
2022-10-25 00:59:14 +0700

    fix

--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221024142849) do
+ActiveRecord::Schema.define(version: 20221024142843) do
 
   create_table "moviegoers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
