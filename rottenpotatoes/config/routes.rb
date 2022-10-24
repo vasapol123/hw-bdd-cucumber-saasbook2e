15c4c38f40b90d6918f200885c8f8af27cc1de3e
mooorkey <i.morkey@msn.com>
2022-10-25 00:31:30 +0700

    Authentication and SSO

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,4 +1,9 @@
 Rottenpotatoes::Application.routes.draw do
+  devise_for :moviegoers, controllers: {
+    omniauth_callbacks: 'moviegoers/omniauth_callbacks',
+    sessions: 'moviegoers/sessions',
+    registrations: 'moviegoers/registrations'
+  }
   resources :movies
 
   post '/movies/add' => 'movies#add', :as => 'add'
